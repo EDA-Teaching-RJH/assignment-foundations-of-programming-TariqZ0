999dea942cd985f8d01eb981826acdef0f292223
TariqZ0 <ta18@kent.ac.uk>
2026-02-04 16:18:40 +0000

    Feature 1: Initialize database with 5 crew members

---
+++ fleet_manager.py
@@ -0,0 +1,6 @@
+def init_database():
+    names = ["Picard", "Riker", "Data", "Worf", "La Forge"]
+    ranks = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Lieutenant"]
+    divs = ["Command", "Command", "Operations", "Security", "Engineering"]
+    ids = [1001, 1002, 1003, 1004, 1005]
+    return names, ranks, divs, ids
